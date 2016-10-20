6a98cab9103cd165b12e1113b795b4e8f417f1a2
katgurdak <katgurdak@gmail.com>
2016-10-20 15:00:24 -0700

    Added userSchema and filled out config/db.js

--- app/models/users.js
+++ app/models/users.js
@@ -1,1 +1,21 @@
 "use strict";
+
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+  username: { type: String, required: true, index: { unique: true }},
+  password: { type: String, required: true},
+  firstname: String,
+  lastname: String,
+  loc: String,
+  picLink: String,
+  dogname: String,
+  dogLikes: [String],
+  dogBreed: String,
+  dogAge: Number
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
--- config/db.js
+++ config/db.js
@@ -1,1 +1,16 @@
 'use strict';
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/okplutodb');
+
+var db = mongoose.connection;
+
+db.on('error', function(err) {
+	console.log('Rut roh: ', err);
+});
+
+db.once('open', function() {
+	console.log('Bark bark! Db is working!');
+});
+
+module.exports = db;
