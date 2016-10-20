d445c5326694b56a2789472dc6f8e50a89622097
jkennedy3161 <kennedyjt88@gmail.com>
2016-10-20 12:25:29 -0700

    'Server is running and served static files in public folder.'

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+// setting port 
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+	console.log('server running! :)');
+})
+
+// serving static files
+app.use(express.static(__dirname + '/public'));
