63318294dcf30d58978c13166ddd8c0135dbab25
Ivey Topaz <iveytopaz@gmail.com>
2016-10-20 19:24:24 -0400

    adding bootstrap to html

--- public/bundle.js
+++ public/bundle.js
@@ -31026,6 +31026,8 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var React = require('react');
+
 var App = function (_React$Component) {
   _inherits(App, _React$Component);
 
@@ -31040,8 +31042,10 @@ var App = function (_React$Component) {
 
 window.App = App;
 
-},{}],176:[function(require,module,exports){
+},{"react":172}],176:[function(require,module,exports){
 "use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -31049,18 +31053,31 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var App = function (_React$Component) {
-  _inherits(App, _React$Component);
+var React = require('react');
 
-  function App() {
-    _classCallCheck(this, App);
+var Signin = function (_React$Component) {
+  _inherits(Signin, _React$Component);
 
-    return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+  function Signin(props) {
+    _classCallCheck(this, Signin);
+
+    return _possibleConstructorReturn(this, (Signin.__proto__ || Object.getPrototypeOf(Signin)).call(this, props));
   }
 
-  return App;
+  _createClass(Signin, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement("form", null)
+      );
+    }
+  }]);
+
+  return Signin;
 }(React.Component);
 
-window.App = App;
+module.exports = Signin;
 
-},{}]},{},[173,174,175,176]);
+},{"react":172}]},{},[173,174,175,176]);
