9734ac7deceece269d98e21449cddb372329f5c5
Ivey Topaz <iveytopaz@gmail.com>
2016-10-21 15:04:15 -0400

    starting authentication routes

--- src/app.jsx
+++ src/app.jsx
@@ -9,10 +9,33 @@ import Home from './components/home.jsx';
 import Signin from './components/signin.jsx';
 import Signup from './components/signup.jsx';
 
+const auth = new AuthService('RdbZPMWBq889RuecM7WKv5q8oli0qwN2', 'katgurdak.auth0.com')
+console.log(auth.login)
+
+const requireAuth = (nextState, replace) => {
+  if (!auth.loggedIn()) {
+    replace ({ pathname: '/signin'})
+  }
+}
+
+export const makeMainRoutes = () => {
+  return (
+    <Route path="/" component={Container} auth={auth}>
+      <IndexRedirect to="/home" />
+      <Route path="home" component={Home} onEnter={requireAuth} />
+      <Route path="/signin" component={Signin} />
+    </Route>
+  )
+}
+
+export default makeMainRoutes
+
 ReactDOM.render(
   <Router>
-    <Route path="/" component={Home}></Route>
-    <Route path="/signup" component={Signup}></Route>
-    <Route path="/signin" component={Signin}></Route>
+
   </Router>, $('#app')[0]
 );
+
+//<Route path="/" component={Home}></Route>
+    // <Route path="/signup" component={Signup}></Route>
+    // <Route path="/signin" component={Signin}></Route>
