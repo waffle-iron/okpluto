824ba756794c1a5c1540d2da0e70d01dfdb7648a
jkennedy3161 <kennedyjt88@gmail.com>
2016-10-20 17:57:07 -0700

    'placed GET and POST for signin and signup in the routes.js folder.'

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,44 @@
 "use strict";
 
+var User = require('./app/models/users');
 
 module.exports = function(app) {
+
+	// signup GET and POST requests for /api/users
+
+	app.get('/signup', function(req, res) {
+		res.end('our signup page will display here'); // react-route will handle this once it's running.
+	});
+
+	app.post('/signup', function(req, res) {
+		var username = req.body.username;
+		var password = req.body.password;
+
+		// Querying through the db
+			// if there is no username with a user model in our database
+				// assign the new model
+				// save the user to our database
+					// create session here
+				// else redirect the user to signup page
+		})
+
+	// signin GET anf POST requests for /api/users
+
+	app.get('signin', function(req, res) {
+		res.end('our signin page will display here');
+	});
+
+	app.post('/signin', function(req, res) {
+		var username = req.body.username;
+		var password = req.body.password;
+
+		// Querying through the db
+		
+			// if the user is not in the database
+				// redirect to signup
+				// compare password from database
+				  // if there is a match, create a session
+				  // else redirect back to signup
+	});
 
 };
--- server.js
+++ server.js
@@ -9,7 +9,6 @@ var port = process.env.PORT || 8080;
 
 // config files
 var db = require('./config/db');
-var User = require('./app/models/users');
 
 // serving static files
 app.use(express.static(__dirname + '/public'));
@@ -21,42 +20,6 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 // routes
 require('./app/routes/routes')(app)
-
-
-// signup GET and POST requests for /api/users
-
-app.get('/signup', function(req, res) {
-	res.end('our signup page will display here');
-});
-
-app.post('/signup', function(req, res) {
-	var username = req.body.username;
-	var password = req.body.password;
-
-	// Querying through the db
-	new User({username : username}).fetch()
-	.then(function(user) {
-		if (!user) {
-			var newUser = new User({
-				username : username,
-				password : password
-			});
-
-			// save the user to our database
-			newUser.save()
-			.then(function(newUser){
-				// create session here
-			})
-		} else {
-			console.log('that name is already being used');
-			res.redirect('/signup');
-		}
-	})
-
-	
-
-
-});
 
 app.listen(port, function() {
 	console.log('server running! :)');
