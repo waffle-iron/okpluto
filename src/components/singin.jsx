df24e3e378427e149f74940e00dd3b0048fd3a70
Ivey Topaz <iveytopaz@gmail.com>
2016-10-20 21:14:57 -0400

    basic signin form

--- src/components/singin.jsx
+++ src/components/singin.jsx
@@ -5,14 +5,43 @@ var React = require('react');
 class Signin extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+  }
+
+  handleChangeUsername(event) {
+    console.log('should change username state')
+    this.setState({username: event.target.value});
+  }
+
+  handleChangePassword(event) {
+    this.setState({password: event.target.value});
   }
 
   render() {
     return (
-      <div>
-        <form>
-        </form>
+      <div className="jumbotron">
+        <div className="row">
+          <label>Username:</label>
+          <input
+            type="text"
+            onChange={this.handleChangeUsername}
+          />
+          </div>
+          <div className="row">
+          <label>Password:</label>
+          <input
+            type="text"
+            onChange={this.handleChangePassword}
+          />
+          </div>
+          <button value="submit">Submit</button>
       </div>
+
     )
   }
 }
