b491f4e1b8bd48d45a9c1d4ce5f4533c530d6914
Ivey Topaz <iveytopaz@gmail.com>
2016-10-23 18:30:11 -0400

    combining grunt tasks concurrently, adding auto launch on boot

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,15 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    concurrent: {
+      target: {
+        tasks: ['nodemon', 'watch'],
+        options: {
+            logConcurrentOutput: true
+        }
+      }
+    },
+
     watch: {
       browserify: {
         files: ['src/**/*.jsx'],
@@ -25,12 +34,34 @@ module.exports = function(grunt) {
         src: ['src/**/*.jsx'],
         dest: 'public/bundle.js'
       }
-    }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'server.js',
+        options: {
+          ignore: ['node_modules/**'],
+          callback: function (nodemon) {
+
+            nodemon.on('config:update', function () {
+            // Delay before server listens on port
+            setTimeout(function() {
+              require('open')('http://localhost:8080');
+            }, 1000);
+            });
+
+          }
+        }
+      }
+    },
+
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
 
-  grunt.registerTask('default', ['build', 'watch']);
+  grunt.registerTask('default', ['build', 'concurrent:target']);
   grunt.registerTask('build', ['browserify']);
 }
