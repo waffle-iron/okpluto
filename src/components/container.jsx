6a2dd3a7cf57d0407359c606adcd9ebbdb70b94d
Ivey Topaz <iveytopaz@gmail.com>
2016-10-23 20:24:01 -0400

    adding post with id token to backend

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,8 +1,29 @@
 import React, { PropTypes as T } from 'react'
 import { Jumbotron } from 'react-bootstrap'
-
+import AuthService from '../utils/AuthService.jsx'
 
 export class Container extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: ''
+    }
+  }
+
+  saveUser(idToken) {
+    $.ajax({
+      url: '/signin',
+      type: 'POST',
+      data: JSON.stringify(idToken),
+      success: data => {
+        console.log(data)
+        this.setState({
+          username: data.username
+        })
+      }
+    })
+  }
+
   render() {
     let children = null;
     if (this.props.children) {
